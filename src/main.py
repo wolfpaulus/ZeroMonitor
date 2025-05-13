0ef403bf41bdd98be2de5ed72e53b2840e070e00
wolf <wolf@paulus.com>
2025-05-13 14:51:03 -0700

    initial commit, just to verify dependencies on remote client

---
+++ src/main.py
@@ -0,0 +1,12 @@
+from pi5neo import Pi5Neo
+
+# Initialize the Pi5Neo class with 10 LEDs and an SPI speed of 800kHz
+neo = Pi5Neo('/dev/spidev0.0', 16, 800)
+
+# Fill the strip with a red color
+neo.fill_strip(64, 0, 0)
+neo.update_strip()  # Commit changes to the LEDs
+
+# Set the 5th LED to blue
+neo.set_led_color(4, 0, 0, 32)
+neo.update_strip()
