6aa8c66bece34660b821aa86f6125f267eec137e
wolf <wolf@paulus.com>
2025-05-26 16:49:02 -0700

    Using ChainMap ;-) makes for nicer code.

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ Zero-Monitor Main Module
 """
 
 from time import sleep
+from collections import ChainMap
 from yaml import safe_load
 from monitor import Connection, Monitor
 from rpi_ws281x import PixelStrip, Color
@@ -35,40 +36,35 @@ if __name__ == "__main__":
         logger.error(f"Error loading configuration file. {err}")
         exit(1)
     try:
-        strip = PixelStrip(num=32, pin=18, freq_hz=800_000, dma=10, invert=False, brightness=config.get("brightness",16), channel=0)
+        strip = PixelStrip(num=32, pin=18, freq_hz=800_000, dma=10, invert=False,
+                           brightness=config.get("brightness", 16), channel=0)
         strip.begin()
     except Exception as err:
         logger.error(f"Error connecting to neopixels: {err}")
         exit(1)
 
     while True:
-        for h, host in enumerate(config.get("hosts")): # iterate over hosts currently 7 configured
-            strip.setPixelColor(0,Color(1,10,7) if h%2 == 0 else Color(0, 0, 0))
+        for h, host in enumerate(config.get("hosts")):  # iterate over hosts currently 7 configured
+            strip.setPixelColor(0, COLORS[0] if h % 2 == 0 else COLORS[-1])
             conn = Connection(host.get("hostname"))  # use with statement
             if conn and conn.client:
-                for i, sensor in enumerate(config.get("sensors").values()): # iterate over sensors
-                    sensor = sensor.copy()
-                    # update the sensor with host overrides
-                    sensor_name = sensor.get("name")
-                    if specific_sensor := host.get(sensor_name):
-                        for k,v in specific_sensor.items():
-                            sensor[k]= v
-                    instance = Monitor.create_instance(sensor_name, conn.client, sensor.get("cmd"), sensor.get("values"))
+                for i, sensor in enumerate(config.get("sensors").values()):  # iterate over sensors
+                    name = sensor.get("name")
+                    sensor = ChainMap(host.get(name,{}), sensor)
+                    instance = Monitor.create_instance(name, conn.client, sensor.get("cmd"), sensor.get("values"))
                     pix = (ROWS - i) * COLS - h - 1
-                    strip.setPixelColor(pix, Color(0, 0, 0))
+                    strip.setPixelColor(pix, COLORS[-1])
                     strip.show()  # activity indicator
                     if instance is not None:
                         strip.setPixelColor(pix, COLORS[instance.probe()])
+                        strip.show()
                     else:
-                        logger.error(f"Sensor {sensor_name} not found. Skipping sensor probe for this host.")
-                        strip.setPixelColor(pix, Color(0, 0, 0))
+                        logger.error(f"Sensor {name} not found. Skipping sensor probe for this host.")
                     sleep(config.get("sensor_timeout", 0.5))
-                    strip.show()
                 conn.close()
             else:
                 logger.warning(f"{host} seems to be offline. Skipping sensor probe(s) for this host.")
                 for i in range(ROWS):
-                    strip.setPixelColor((ROWS - i) * COLS - h - 1, Color(0, 0, 0))
+                    strip.setPixelColor((ROWS - i) * COLS - h - 1, COLORS[-1])
                 strip.show()
             sleep(config.get("host_timeout", 0.5))
-
